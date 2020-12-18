d794aab5009041822a114e65579ec7b194576e30
Luca Voges <info@sociant.de>
2020-12-18 01:25:44 +0100

    Completed first version of event manager with EventEntities (as instances, kind of singleton for each object)
    Added Event Emitter, Listener, Remove, Existence Check, ...
    
    Added Unit Tests for common and quite uncommon scenarios

--- src/main/java/de/fhe/ai/manager/EventManager.java
+++ src/main/java/de/fhe/ai/manager/EventManager.java
@@ -1,7 +1,15 @@
 package de.fhe.ai.manager;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class EventManager {
+
     private static EventManager INSTANCE;
+
+    private final Map<Object,EventEntity> boundEventEntities = new HashMap<>();
 
     public static EventManager getInstance() {
         if (INSTANCE == null)
@@ -9,7 +17,114 @@ public class EventManager {
         return INSTANCE;
     }
 
-    private EventManager() {
+    private EventManager() {}
+
+    public EventEntity getEventEntity(Object object) {
+        if(boundEventEntities.containsKey(object))
+            return boundEventEntities.get(object);
+
+        EventEntity eventEntity = new EventEntity(object);
+        boundEventEntities.put(object,eventEntity);
+
+        return eventEntity;
+    }
+
+    public void removeEventEntity(EventEntity eventEntity) {
+        boolean removed = boundEventEntities.entrySet()
+                .removeIf(entry -> entry.getValue().equals(eventEntity));
+
+        if(removed) eventEntity.removeAllListeners();
+    }
+
+    public void removeEventEntityByObject(Object object) {
+        if(boundEventEntities.containsKey(object))
+            this.removeEventEntity(boundEventEntities.get(object));
+    }
+
+    public void emitToAllEntitiesOfType(Class<?> target, String event, Object data) {
+        boundEventEntities
+                .entrySet()
+                .stream()
+                .filter(entry -> entry.getKey().getClass().equals(target))
+                .forEach(entry -> entry.getValue().emit(event,data));
+    }
+
+    public static class EventEntity {
+
+        private final Object entity;
+        private final Map<String, ArrayList<EventCallback>> callbacks = new HashMap<>();
+
+        public EventEntity(Object entity) {
+            this.entity = entity;
+        }
+
+        public Object getEntity() {
+            return entity;
+        }
+
+        public void addListener(String event, EventCallback eventCallback) {
+            String targetEvent = event == null ? "" : event;
+
+            ArrayList<EventCallback> eventCallbacks = null;
+            if(callbacks.containsKey(targetEvent)) eventCallbacks = callbacks.get(targetEvent);
+            else {
+                eventCallbacks = new ArrayList<>();
+                callbacks.put(targetEvent, eventCallbacks);
+            }
+
+            eventCallbacks.add(eventCallback);
+        }
+
+        public void addListener(EventCallback eventCallback) {
+            this.addListener(null,eventCallback);
+        }
+
+        public void removeListener(String event, EventCallback eventCallback) {
+            String targetEvent = event == null ? "" : event;
+
+            if(callbacks.containsKey(targetEvent)) {
+                ArrayList<EventCallback> eventCallbacks = callbacks.get(targetEvent);
+                eventCallbacks.remove(eventCallback);
+            }
+        }
+
+        public void removeListener(EventCallback eventCallback) {
+            this.removeListener(null,eventCallback);
+        }
+
+        public void removeAllListeners(String event) {
+            callbacks.remove(event);
+        }
+
+        public void removeAllListeners() {
+            callbacks.clear();
+        }
+
+        public boolean isListenerRegistered(EventCallback eventCallback) {
+            return callbacks
+                    .values()
+                    .stream()
+                    .anyMatch(list -> list.contains(eventCallback));
+        }
+
+        public void emit(String event, Object data) {
+            if(callbacks.containsKey(event))
+                callbacks.get(event).forEach(callback -> callback.onEvent(entity,event,data));
+
+            //Emits event again to listeners receiving all events
+            if(!event.equals("") && callbacks.containsKey(""))
+                callbacks.get("").forEach(callback -> callback.onEvent(entity,event,data));
+        }
+
+        public void emit(String event) {
+            this.emit(event,null);
+        }
+
+    }
+
+    public interface EventCallback {
+
+        void onEvent(Object entity, String event, Object data);
 
     }
 }
---
+++ src/test/java/de/fhe/ai/manager/EventManagerTest.java
@@ -0,0 +1,166 @@
+package de.fhe.ai.manager;
+
+import de.fhe.ai.model.Line;
+import de.fhe.ai.model.PassengerTram;
+import de.fhe.ai.model.Tram;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+public class EventManagerTest {
+
+    @Test
+    public void get_single_event_entity_instance() {
+        Tram exampleTramObject = new PassengerTram(0,100,1000,100,"passenger");
+        EventManager eventManager = EventManager.getInstance();
+
+        EventManager.EventEntity firstEventEntity = eventManager.getEventEntity(exampleTramObject);
+        EventManager.EventEntity secondEventEntity = eventManager.getEventEntity(exampleTramObject);
+
+        Assert.assertEquals("The objects should be the same",firstEventEntity,secondEventEntity);
+    }
+
+    @Test
+    public void add_listener_to_event_entity() {
+        Tram exampleTramObject = new PassengerTram(0,100,1000,100,"passenger");
+        EventManager eventManager = EventManager.getInstance();
+
+        EventManager.EventEntity eventEntity = eventManager.getEventEntity(exampleTramObject);
+
+        String demoEvent = "helloWorld";
+        boolean demoData = true;
+
+        //Listener for all events
+        eventEntity.addListener((entity, event, data) -> {
+            System.out.println("Event received with identifier " + event);
+
+            Assert.assertEquals("Event entity should be the same object",exampleTramObject,entity);
+            Assert.assertEquals("Event identifier should be the same",demoEvent,event);
+            Assert.assertEquals("Event data should be the same",demoData,data);
+        });
+
+        eventEntity.emit(demoEvent,demoData);
+
+        //Remove event entity from manager for other tests
+        eventManager.removeEventEntity(eventEntity);
+    }
+
+    @Test
+    public void test_different_listener_targets() {
+        Tram exampleTramObject = new PassengerTram(0,100,1000,100,"passenger");
+        EventManager eventManager = EventManager.getInstance();
+
+        EventManager.EventEntity eventEntity = eventManager.getEventEntity(exampleTramObject);
+
+        String firstEvent = "create";
+        String secondEvent = "destroy";
+
+        EventManager.EventCallback firstCallback = (entity, event, data) -> {
+            System.out.println("Received event with identifier " + event);
+            Assert.assertEquals("Event should be create",firstEvent,event);
+        };
+
+        EventManager.EventCallback secondCallback = (entity, event, data) -> {
+            System.out.println("Received event with identifier " + event);
+            Assert.assertEquals("Event should be destroy",secondEvent,event);
+        };
+
+        eventEntity.addListener(firstEvent,firstCallback);
+        eventEntity.addListener(secondEvent,secondCallback);
+
+        eventEntity.emit(firstEvent);
+        eventEntity.emit(secondEvent);
+
+        //Remove event entity from manager for other tests
+        eventManager.removeEventEntity(eventEntity);
+    }
+
+    @Test
+    public void check_listener_existence() {
+        Tram exampleTramObject = new PassengerTram(0,100,1000,100,"passenger");
+        EventManager eventManager = EventManager.getInstance();
+
+        EventManager.EventEntity eventEntity = eventManager.getEventEntity(exampleTramObject);
+
+        EventManager.EventCallback callback = (entity, event, data) -> {};
+
+        eventEntity.addListener(callback);
+
+        Assert.assertTrue("Callback should still be registered", eventEntity.isListenerRegistered(callback));
+
+        eventEntity.removeListener(callback);
+
+        Assert.assertFalse("Callback should no longer be registerd", eventEntity.isListenerRegistered(callback));
+
+        //Remove event entity from manager for other tests
+        eventManager.removeEventEntity(eventEntity);
+    }
+
+    @Test
+    public void remove_entity_and_listener() {
+        Tram exampleTramObject = new PassengerTram(0,100,1000,100,"passenger");
+        EventManager eventManager = EventManager.getInstance();
+
+        EventManager.EventEntity eventEntity = eventManager.getEventEntity(exampleTramObject);
+
+        EventManager.EventCallback callback = (entity, event, data) -> {};
+
+        eventEntity.addListener(callback);
+
+        eventManager.removeEventEntityByObject(exampleTramObject);
+
+        Assert.assertFalse("Event should no longer be registered",eventEntity.isListenerRegistered(callback));
+
+        EventManager.EventEntity otherEntity = eventManager.getEventEntity(exampleTramObject);
+
+        Assert.assertNotEquals("Entites should be distinct",eventEntity,otherEntity);
+
+        //Remove event entity from manager for other tests
+        eventManager.removeEventEntity(otherEntity);
+    }
+
+
+    @Test
+    public void test_multiple_entities_and_listener() {
+        Tram firstTramObject = new PassengerTram(0,100,1000,100,"passenger");
+        Tram secondTramObject = new PassengerTram(0,100,1000,100,"passenger");
+        Line lineObject = new Line("Linie 6", new ArrayList<>());
+
+        EventManager eventManager = EventManager.getInstance();
+
+        EventManager.EventEntity firstTramEventEntity = eventManager.getEventEntity(firstTramObject);
+        EventManager.EventEntity secondTramEventEntity = eventManager.getEventEntity(secondTramObject);
+        EventManager.EventEntity lineEventEntity = eventManager.getEventEntity(lineObject);
+
+        String tramEvent = "createTram";
+        String lineEvent = "createLine";
+
+        firstTramEventEntity.addListener((entity, event, data) -> {
+            System.out.println("Received event for firstTram with identifier " + event);
+
+            Assert.assertEquals("Event identifier should be createTram",tramEvent,event);
+        });
+
+        secondTramEventEntity.addListener((entity, event, data) -> {
+            System.out.println("Received event for secondTram with identifier " + event);
+
+            Assert.assertEquals("Event identifier should be createTram",tramEvent,event);
+        });
+
+        lineEventEntity.addListener((entity, event, data) -> {
+            System.out.println("Received event for line with identifier " + event);
+
+            Assert.assertEquals("Event identifier should be createLine",lineEvent,event);
+        });
+
+        eventManager.emitToAllEntitiesOfType(PassengerTram.class,tramEvent,null);
+        eventManager.emitToAllEntitiesOfType(Line.class,lineEvent,null);
+
+        //Remove event entity from manager for other tests
+        eventManager.removeEventEntity(firstTramEventEntity);
+        eventManager.removeEventEntity(secondTramEventEntity);
+        eventManager.removeEventEntity(lineEventEntity);
+    }
+
+}

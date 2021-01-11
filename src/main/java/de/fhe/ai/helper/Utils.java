f57c7fa0753e2bcc6d243fff31ff2aa2b6234709
Erik Bünnig <Erikbuennig@gmail.com>
2021-01-11 22:38:47 +0100

    extraction of model base reflection to Utils

--- src/main/java/de/fhe/ai/helper/Utils.java
+++ src/main/java/de/fhe/ai/helper/Utils.java
@@ -1,5 +1,14 @@
 package de.fhe.ai.helper;
 
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+import de.fhe.ai.model.ModelBase;
+
 /**
  * Class with static helper methods.
  */
@@ -18,5 +27,101 @@ public final class Utils {
         return fullClassName.contains(".") && fullClassName.lastIndexOf('.') + 1 < fullClassName.length()
                 ? fullClassName.substring(fullClassName.lastIndexOf('.') + 1, fullClassName.length())
                 : fullClassName;
+    }
+
+    /**
+     * Indents each line in a string by the given depth
+     * 
+     * @param stringToIndent the string to indent each line for
+     * @param depth          the depth of indentation where one indentation is four
+     *                       spaces
+     * @return a new string with the specified indentation
+     */
+    public static String indentEachLine(String stringToIndent, int depth) {
+        // java internal String.replace does not work for whatever reason
+        // return stringToIndent.replace("\n", "\n" + indentation);
+
+        String output = "";
+        var subStr = stringToIndent.split("\n");
+        for (int ii = 0; ii < depth; ii++) {
+            for (int jj = 0; jj < depth; jj++) {
+                output += "    ";
+            }
+            output += subStr[ii];
+        }
+
+        return output;
+    }
+
+    /**
+     * Returns a string that represents the given model, this method will show
+     * private fields, their types and some additional information if necessary.
+     * This method should ideally only used for debugging as it can expose sensitive
+     * information and lead to massive outputs with the correct arguments
+     * 
+     * @param modelBase       the model to get a verbsoe representation of
+     * @param depth           the maximum depth at which to verbosely display
+     * @param showCollections indicates whether collections should be completely
+     *                        displayed non-primitive fields
+     * @return a verbose json-like representation of the model
+     */
+    public static String getVerboseModelRepresentation(ModelBase modelBase, int depth) {
+        return getVerboseModelRepresentation(modelBase, depth, 0);
+    }
+
+    private static String getVerboseModelRepresentation(ModelBase modelBase, int targetDepth, int currentDepth) {
+        // get underlying type of the deriving model
+        Class<?> modelType = modelBase.getClass();
+        List<Field> allFields = new ArrayList<>();
+
+        // get all fields by moving up the class hierarchy
+        Class<?> superType = modelType;
+        while (superType != null) {
+            Collections.addAll(allFields, superType.getDeclaredFields());
+            superType = superType.getSuperclass();
+        }
+
+        // prepare output
+        StringBuilder outputBuilder = new StringBuilder(
+                Utils.getShortClassName(modelType) + "(" + modelBase.getId() + ") {\n");
+
+        // appaned all fields with name and value and one indentation
+        for (Field field : allFields) {
+            StringBuilder fieldLineBuilder = new StringBuilder(field.getName() + ": ");
+            try {
+                field.setAccessible(true);
+                Object fieldValue = field.get(modelBase);
+
+                if (fieldValue != null) {
+                    // append field name and value
+                    // view count on collections
+                    if (fieldValue instanceof Collection<?>) {
+                        fieldLineBuilder.append("{ Count: " + ((Collection<?>) fieldValue).size() + " }");
+                    } else if (fieldValue instanceof Map<?, ?>) {
+                        fieldLineBuilder.append("{ Count: " + ((Map<?, ?>) fieldValue).size() + " }");
+                    } else if (fieldValue instanceof ModelBase) {
+                        if (targetDepth > 0) {
+                            fieldLineBuilder.append(Utils.getVerboseModelRepresentation((ModelBase) fieldValue,
+                                    targetDepth - currentDepth));
+                        } else {
+                            fieldLineBuilder.append(Utils.getShortClassName(fieldValue.getClass()) + "("
+                                    + ((ModelBase) fieldValue).getId() + ") { ... }");
+                        }
+                    } else if (fieldValue instanceof String) {
+                        fieldLineBuilder.append("'" + fieldValue + "'");
+                    } else {
+                        fieldLineBuilder.append(fieldValue.toString());
+                    }
+                } else {
+                    fieldLineBuilder.append("null");
+                }
+            } catch (Exception ex) {
+                fieldLineBuilder.append("UnknownType = UnknownValue");
+            }
+            // add indentation for fieldLine according to targetDepth
+            outputBuilder.append(Utils.indentEachLine(fieldLineBuilder.toString(), currentDepth + 1) + "\n");
+        }
+
+        return outputBuilder.append('}').toString();
     }
 }
--- src/main/java/de/fhe/ai/model/ModelBase.java
+++ src/main/java/de/fhe/ai/model/ModelBase.java
@@ -1,14 +1,11 @@
 package de.fhe.ai.model;
 
-import java.lang.reflect.*;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
-
 import de.fhe.ai.helper.Utils;
 import de.fhe.ai.manager.EventManager;
 
+/**
+ * Provides a base class for all models
+ */
 public abstract class ModelBase {
 
     private final int id;
@@ -23,51 +20,9 @@ public abstract class ModelBase {
         return id;
     }
 
-    protected String getString(ModelBase modelBase) {
-        // get underlying type of the deriving model
-        Class<?> modelType = modelBase.getClass();
-        List<Field> allFields = new ArrayList<>();
-
-        // get all fields dynamically by moving up the class hierarchy
-        Class<?> superType = modelType;
-        while (superType != null) {
-            Collections.addAll(allFields, superType.getDeclaredFields());
-            superType = superType.getSuperclass();
-        }
-
-        // prepare output
-        String output = Utils.getShortClassName(modelType) + "\n";
-
-        // appaned all fields with name and value and one indentation
-        for (Field field : allFields) {
-            output += "    " + field.getName() + ": ";
-            try {
-                field.setAccessible(true);
-                var value = field.get(modelBase);
-
-                // append field name and value
-                if (value != null) {
-                    // view count on collections and id on moduleBase (prevents nesting)
-                    // else toString()
-                    if (value instanceof Collection) {
-                        output += "Collection (Count: " + ((Collection<?>) value).size() + ")";
-                    } else if (value instanceof String) {
-                        output += "'" + value + "'";
-                    } else if (value instanceof ModelBase) {
-                        output += Utils.getShortClassName(value.getClass()) + " (Id: " + ((ModelBase) value).getId()
-                                + ")";
-                    } else {
-                        output += value.toString();
-                    }
-                } else {
-                    output += "null";
-                }
-            } catch (Exception ex) {
-                output += "Unknown";
-            }
-            output += "\n";
-        }
-
-        return output;
+    @Override
+    public String toString() {
+        // TODO: only return public fields in non-debug enviroments
+        return Utils.getVerboseModelRepresentation(this, 0);
     }
 }

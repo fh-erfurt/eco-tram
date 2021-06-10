86c65e115b06b23d9f9dafff8bcd3cc31a11fdfa
Luca Voges <vogeslu@gmail.com>
2021-06-10 10:44:41 +0200

    fixed wrong imports

--- backend/src/main/java/de/ecotram/backend/repository/PassengerTramRepository.java
+++ backend/src/main/java/de/ecotram/backend/repository/PassengerTramRepository.java
@@ -1,9 +1,9 @@
 package de.ecotram.backend.repository;
 
-import de.ecotram.backend.entity.network.PassengerTram;
+import de.ecotram.backend.entity.PassengerTram;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PassengerTram extends JpaRepository<PassengerTram, Long> {
+public interface PassengerTramRepository extends JpaRepository<PassengerTram, Long> {
 }

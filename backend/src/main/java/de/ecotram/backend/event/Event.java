1536f4f4cca88ddad6a3522fd979c9197564407f
erxkk <ErikBuennig@gmail.com>
2021-06-27 16:08:50 +0200

    changed event invoke to throw if no error handler is given

--- backend/src/main/java/de/ecotram/backend/event/Event.java
+++ backend/src/main/java/de/ecotram/backend/event/Event.java
@@ -4,18 +4,18 @@ import lombok.Getter;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.function.Consumer;
 
 public final class Event<T extends EventArgs> {
-    private final List<Callback<T>> subscribers = new ArrayList<>();
+    private final List<Consumer<T>> consumers = new ArrayList<>();
     private final ErrorHandler<T> errorHandler;
 
     @Getter
     private final Accessor access = new Accessor();
 
     public Event() {
-        this.errorHandler = (e, args) -> {
-        };
+        this.errorHandler = null;
     }
 
     public Event(ErrorHandler<T> errorHandler) {
@@ -27,42 +27,40 @@ public final class Event<T extends EventArgs> {
     }
 
     public synchronized void invoke(T args) {
-        for (Callback<T> subscriber : subscribers) {
+        for (Consumer<T> subscriber : consumers) {
             try {
-                subscriber.OnEvent(args);
+                subscriber.accept(args);
             } catch (Exception ex) {
+                if (errorHandler == null)
+                    throw ex;
+
                 errorHandler.OnException(ex, args);
             }
         }
     }
 
-    private synchronized void internalAdd(Callback<T> handler) {
-        this.subscribers.add(handler);
+    private synchronized void internalAdd(Consumer<T> consumer) {
+        this.consumers.add(consumer);
     }
 
-    private synchronized void internalRemove(Callback<T> handler) {
-        this.subscribers.remove(handler);
+    private synchronized void internalRemove(Consumer<T> consumer) {
+        this.consumers.remove(consumer);
     }
 
     // only expose this to consumers, not the event itself
     // see @test.java.de.ecotram.backend.event.EventTests for example
     public final class Accessor {
-        public void add(Event.Callback<T> handler) {
-            internalAdd(handler);
+        public void add(Consumer<T> consumer) {
+            internalAdd(consumer);
         }
 
-        public void remove(Event.Callback<T> handler) {
-            internalRemove(handler);
+        public void remove(Consumer<T> consumer) {
+            internalRemove(consumer);
         }
 
-        public ErrorHandler<T> getErrorHandler() {
-            return errorHandler;
+        public Optional<ErrorHandler<T>> getErrorHandler() {
+            return errorHandler != null ? Optional.of(errorHandler) : Optional.empty();
         }
-    }
-
-    @FunctionalInterface
-    public interface Callback<T extends EventArgs> {
-        void OnEvent(T args);
     }
 
     @FunctionalInterface

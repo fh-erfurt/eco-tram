75cd77cb24ac81e92e601d9ea5c5ce14000088f9
erxkk <ErikBuennig@gmail.com>
2021-08-17 12:19:11 +0200

    Removed TODOs

--- backend/src/main/java/de/ecotram/backend/BackendServerNewApplication.java
+++ backend/src/main/java/de/ecotram/backend/BackendServerNewApplication.java
@@ -3,9 +3,6 @@ package de.ecotram.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-// TODO: add sql script + update documentation to include in installation steps
-// TODO(erik): unit tests
-
 @SpringBootApplication
 public class BackendServerNewApplication {
     public static void main(String[] args) {
